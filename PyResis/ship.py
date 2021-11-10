135e120c85c6826f90642b27164f47218629618e
Philipp Kraus <philipp.kraus@flashpixx.de>
2021-11-10 22:46:34 +0100

    restructure package, update infrastructure

--- PyResis/ship.py
+++ PyResis/ship.py
@@ -1,16 +1,12 @@
-import os
 import math
+
 import numpy as np
 from scipy import interpolate
 
-fn = os.path.join(os.path.dirname(__file__), 'cr.txt')
-cr_list = np.loadtxt(fn)
-cr_points = cr_list.T[:3].T
-cr_values = cr_list.T[3].T / 1000
-cr = interpolate.LinearNDInterpolator(cr_points, cr_values)
-cr_nearest = interpolate.NearestNDInterpolator(cr_points, cr_values)
+from PyResis.constants import CR, CRNEAREST
 
-def frictional_resistance_coef(length, speed, **kwargs):
+
+def frictional_resistance_coef(length: float, speed: float, **kwargs):
     """
     Flat plate frictional resistance of the ship according to ITTC formula.
     ref: https://ittc.info/media/2021/75-02-02-02.pdf
@@ -20,11 +16,10 @@ def frictional_resistance_coef(length, speed, **kwargs):
     :param kwargs: optional could take in temperature to take account change of water property
     :return: Frictional resistance coefficient of the vehicle
     """
-    Cf = 0.075 / (np.log10(reynolds_number(length, speed, **kwargs)) - 2) ** 2
-    return Cf
+    return 0.075 / (np.log10(reynolds_number(length, speed, **kwargs)) - 2) ** 2
 
 
-def reynolds_number(length, speed, temperature=25):
+def reynolds_number(length: float, speed: float, temperature: float = 25):
     """
     Reynold number utility function that return Reynold number for vehicle at specific length and speed.
     Optionally, it can also take account of temperature effect of sea water.
@@ -39,11 +34,10 @@ def reynolds_number(length, speed, temperature=25):
     kinematic_viscosity = interpolate.interp1d([0, 10, 20, 25, 30, 40],
                                                np.array([18.54, 13.60, 10.50, 9.37, 8.42, 6.95]) / 10 ** 7)
     # Data from http://web.mit.edu/seawater/2017_MIT_Seawater_Property_Tables_r2.pdf
-    Re = length * speed / kinematic_viscosity(temperature)
-    return Re
+    return length * speed / kinematic_viscosity(temperature)
 
 
-def froude_number(speed, length):
+def froude_number(speed: float, length: float):
     """
     Froude number utility function that return Froude number for vehicle at specific length and speed.
 
@@ -52,12 +46,10 @@ def froude_number(speed, length):
     :return: Froude number of the vehicle (dimensionless)
     """
     g = 9.80665  # conventional standard value m/s^2
-    Fr = speed / np.sqrt(g * length)
-    return Fr
+    return speed / np.sqrt(g * length)
 
 
-
-def residual_resistance_coef(slenderness, prismatic_coef, froude_number):
+def residual_resistance_coef(slenderness: float, prismatic_coef: float, froude_number: float):
     """
     Residual resistance coefficient estimation from slenderness function, prismatic coefficient and Froude number.
 
@@ -66,23 +58,21 @@ def residual_resistance_coef(slenderness, prismatic_coef, froude_number):
     :param froude_number: Froude number of the ship dimensionless 
     :return: Residual resistance of the ship
     """
-    Cr = cr(slenderness, prismatic_coef, froude_number)
+    crvalue = CR(slenderness, prismatic_coef, froude_number)
     if math.isnan(Cr):
-        Cr = cr_nearest(slenderness, prismatic_coef, froude_number)
+        crvalue = CRNEAREST(slenderness, prismatic_coef, froude_number)
 
     # if Froude number is out of interpolation range, nearest extrapolation is used
-    return Cr
+    return crvalue
 
 
-class Ship():
+class Ship:
     """
     Class of ship object, can be initialize with zero argument.
     """
-    def __init__(self):
-        pass
 
-    def dimension(self, length, draught, beam, speed,
-                 slenderness_coefficient, prismatic_coefficient):
+    def __init__(self, length: float, draught: float, beam: float, speed: float,
+                 slenderness_coefficient: float, prismatic_coefficient: float):
         """
         Assign values for the main dimension of a ship.
 
@@ -102,8 +92,7 @@ class Ship():
         self.slenderness_coefficient = slenderness_coefficient
         self.prismatic_coefficient = prismatic_coefficient
         self.displacement = (self.length / self.slenderness_coefficient) ** 3
-        self.surface_area = 1.025 * (1.7 * self.length * self.draught +
-                                     self.displacement / self.draught)
+        self.surface_area = 1.025 * (1.7 * self.length * self.draught + self.displacement / self.draught)
 
     def resistance(self):
         """
@@ -111,22 +100,20 @@ class Ship():
 
         :return: newton the resistance of the ship
         """
-        self.total_resistance_coef = frictional_resistance_coef(self.length, self.speed) + \
+        total_resistance_coef = frictional_resistance_coef(self.length, self.speed) + \
                                 residual_resistance_coef(self.slenderness_coefficient,
                                                          self.prismatic_coefficient,
                                                          froude_number(self.speed, self.length))
-        RT = 1 / 2 * self.total_resistance_coef * 1025 * self.surface_area * self.speed ** 2
-        return RT
+        return 1 / 2 * total_resistance_coef * 1025 * self.surface_area * self.speed ** 2
 
-    def maximum_deck_area(self, water_plane_coef=0.88):
+    def maximum_deck_area(self, water_plane_coef: float = 0.88):
         """
         Return the maximum deck area of the ship
 
         :param water_plane_coef: optional water plane coefficient
         :return: Area of the deck
         """
-        AD = self.beam * self.length * water_plane_coef
-        return AD
+        return self.beam * self.length * water_plane_coef
 
     def get_reynold_number(self):
         """
@@ -136,7 +123,7 @@ class Ship():
         """
         return reynolds_number(self.length, self.speed)
 
-    def prop_power(self, propulsion_eff=0.7, sea_margin=0.2):
+    def prop_power(self, propulsion_eff: float = 0.7, sea_margin: float = 0.2):
         """
         Total propulsion power of the ship.
 
@@ -144,10 +131,4 @@ class Ship():
         :param sea_margin: Sea margin take account of interaction between ship and the sea, e.g. wave
         :return: Watts shaft propulsion power of the ship
         """
-        PP = (1 + sea_margin) * self.resistance() * self.speed/propulsion_eff
-        return PP
-
-if __name__ == '__main__':
-    s1 = Ship()
-    s1.dimension(5.72, 0.248, 0.76, 0.2, 6.99, 0.613)
-    print(s1.prop_power())
+        return (1 + sea_margin) * self.resistance() * self.speed / propulsion_eff
